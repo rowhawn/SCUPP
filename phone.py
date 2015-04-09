a3a6b8b37633576a6a025f2fe4b3201ccecf3e4a
rowhawn <rowhawn@gmail.com>
2015-04-09 00:35:04 +0000

    added rudimentary audio playback

--- phone.py
+++ phone.py
@@ -1,11 +1,7 @@
 import pygame
+import os
 
 class Menu:
-	menuId = ""
-	actionMessage = ""
-	menuMessage = ""
-	submenus = []
-	
 	def __init__(self, menuId, actionMessage, submenus = []):
 		self.menuId = menuId
 		self.actionMessage = actionMessage
@@ -31,11 +27,14 @@ class Menu:
 		if (self.submenus[7] is not None):
 			print("Press 8 to " + self.submenus[7].actionMessage)
 		if (self.submenus[8] is not None):
-			print("Press 9 to " +self.submenus[8].actionMessage)
+			print("Press 9 to " + self.submenus[8].actionMessage)
 		if (self.submenus[9] is not None):
-			print("Press 0 to " +self.submenus[9].actionMessage)
+			print("Press 0 to " + self.submenus[9].actionMessage)
 
-		
+
+homeDir = os.environ['HOME']
+pygame.mixer.init()
+pygame.mixer.music.load(homeDir + "/projects/SCUPP/resources/audio/match0.wav")
 submenu1 = Menu("first submenu", "do the first action", [None, None, None, None, None, None, None, None, None, None])
 submenu2 = Menu("second submenu", "do some other action", [None, None, None, None, None, None, None, None, None, None])
 currMenu = Menu("main menu", "hear the main menu", [submenu1, submenu2, None, None, None, None, None, None, None, None]) 
@@ -44,26 +43,27 @@ submenu2.submenus[9] = currMenu
 
 while 1:
 	currMenu.readMenu()
-	keyPressed = raw_input()
-	if (keyPressed == '1'):
-		currMenu = currMenu.submenus[0]
-	elif (keyPressed == '2'):
+	keyPressed = input()
+	if (keyPressed == '1' and currMenu.submenus[0] is not None):
+                currMenu = currMenu.submenus[0]
+                pygame.mixer.music.play(0)
+	elif (keyPressed == '2' and currMenu.submenus[1] is not None):
 		currMenu = currMenu.submenus[1]
-	elif (keyPressed == '3'):
+	elif (keyPressed == '3' and currMenu.submenus[2] is not None):
 		currMenu = currMenu.submenus[2]
-	elif (keyPressed == '4'):
+	elif (keyPressed == '4' and currMenu.submenus[3] is not None):
 		currMenu = currMenu.submenus[3]
-	elif (keyPressed == '5'):
+	elif (keyPressed == '5' and currMenu.submenus[4] is not None):
 		currMenu = currMenu.submenus[4]
-	elif (keyPressed == '6'):
+	elif (keyPressed == '6' and currMenu.submenus[5] is not None):
 		currMenu = currMenu.submenus[5]
-	elif (keyPressed == '7'):
+	elif (keyPressed == '7' and currMenu.submenus[6] is not None):
 		currMenu = currMenu.submenus[6]
-	elif (keyPressed == '8'):
+	elif (keyPressed == '8' and currMenu.submenus[7] is not None):
 		currMenu = currMenu.submenus[7]
-	elif (keyPressed == '9'):
+	elif (keyPressed == '9' and currMenu.submenus[8] is not None):
 		currMenu = currMenu.submenus[8]
-	elif (keyPressed == '0'):
+	elif (keyPressed == '0' and currMenu.submenus[9] is not None):
 		currMenu = currMenu.submenus[9]
 	elif (keyPressed == '*'):
 		print("you pressed something else!")
@@ -73,3 +73,4 @@ while 1:
 		quit()
 	else:
 		print("invalid input")
+
